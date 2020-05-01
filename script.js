b9d3475d72082e43878917e855abbf639ade1e43
erik hirsch <ehirsch760@gmail.com>
2020-04-30 22:06:13 -0700

    local storage saving not parseing

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var savedTextObj =localStorage.getItem("savedTextObj")? JSON.parse(localStorage.getItem("savedTextObj")) : {};
 console.log("this script is running!");
 $(document).ready(function () {
 
@@ -10,10 +11,10 @@ $(document).ready(function () {
         $('#currentDay').text(currentTimeData);
     };
     //add the past, present, future class to appropriate divs
-    var checkTense = function() {
+    var checkTense = function () {
         //get the current hour
         var currentHour = moment().hours();
-        console.log('currentHour: ',currentHour);
+        console.log('currentHour: ', currentHour);
         //get the time block time attr
         var timeBlockArr = $('.row.time-block');
         //loop through and check to add tense class
@@ -26,19 +27,31 @@ $(document).ready(function () {
                 $targetTimeBlock.find(".description").addClass("past");
             } else if (currentHour < targetTimeAttr) {
                 $targetTimeBlock.find(".description").addClass("future");
-            } else if (currentHour === targetTimeAttr) {
+            } else if (currentHour == targetTimeAttr) {
                 $targetTimeBlock.find(".description").addClass("present");
             }
         }
     }
     //on click of save button store textarea values (stringify will be helpful)
 
+    $(".saveBtn").on("click", function () {
+        event.preventDefault();
+        var parent = this.parentNode;
+        var textInput = parent.childNodes[3];
+        var data = textInput.value
+        var textsave = parent.id;
+        savedTextObj[textsave] = data;
+        localStorage.setItem("savedTextObj", JSON.stringify(savedTextObj))
+       
+        console.log(savedTextObj[textsave] = data)
+        //save input texttext to local storage
+    });
     //on reload of the page get the stored data show it
-
+    console.log(savedTextObj)
     //run the function
-    setInterval(function(){ 
-        setCurrentTimeOnHeader(); 
+    // setInterval(function () {
+        setCurrentTimeOnHeader();
         checkTense();
-    }, 1000);
-    
+    // }, 1000);
+
 });
