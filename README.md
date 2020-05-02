fd65a8e4407ca7a95bede0ae8391c26ced6488b4
erik hirsch <ehirsch760@gmail.com>
2020-05-01 19:56:18 -0700

    finished+ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # DayPlanner
+In this project I have created a day planner. The planner has a time and date that update automatically without the need to refresh the page. The planner is color coded to signify the current time of day. Any time slot that has passed will turn gray, the current time is colored red, and any future time is colored green. Text can be entered into each time slot and can be saved by pressing the button at the end of each time slot row. When the save button is pressed the text is saved to local storage and will be displayed when the page loads.
+![screenshot](./ReadMePics/WorkDayScheduler.png)
+![screenshot](./ReadMePics/WorkDayScheduler2.png)
+![screenshot](./ReadMePics/WorkDayScheduler3.png)
--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-var savedTextObj =localStorage.getItem("savedTextObj")? JSON.parse(localStorage.getItem("savedTextObj")) : {};
-console.log("this script is running!");
+var savedTextObj =localStorage.getItem("savedTextObj") ? JSON.parse(localStorage.getItem("savedTextObj")) : {};
+console.log(savedTextObj);
+
+
 $(document).ready(function () {
+
 
     //check if th document is ready
     console.log("document is ready!");
@@ -12,15 +15,19 @@ $(document).ready(function () {
     };
     //add the past, present, future class to appropriate divs
     var checkTense = function () {
+        // const notes = Object.values(savedTextObj)
         //get the current hour
         var currentHour = moment().hours();
         console.log('currentHour: ', currentHour);
         //get the time block time attr
         var timeBlockArr = $('.row.time-block');
+        // var timeBlockChildren = timeBlockArr.children(".description")
         //loop through and check to add tense class
-        for (i = 0; i < timeBlockArr.length; i++) {
+        for (var i = 0; i < timeBlockArr.length; i++) {
             //index time block
             var $targetTimeBlock = $(timeBlockArr[i]);
+            // var $targetDescription = $(timeBlockChildren[i]).val(notes[i])
+            // console.log($targetDescription)
             //get time attr for each index item
             var targetTimeAttr = $targetTimeBlock.attr('time');
             if (currentHour > targetTimeAttr) {
@@ -33,25 +40,40 @@ $(document).ready(function () {
         }
     }
     //on click of save button store textarea values (stringify will be helpful)
-
+    
+    
     $(".saveBtn").on("click", function () {
         event.preventDefault();
+        //save input texttext of selected button to local storage
         var parent = this.parentNode;
         var textInput = parent.childNodes[3];
         var data = textInput.value
         var textsave = parent.id;
+        var value = $(this).prev().val()
+        console.log(value);
         savedTextObj[textsave] = data;
         localStorage.setItem("savedTextObj", JSON.stringify(savedTextObj))
        
+       
         console.log(savedTextObj[textsave] = data)
-        //save input texttext to local storage
     });
     //on reload of the page get the stored data show it
     console.log(savedTextObj)
     //run the function
-    // setInterval(function () {
+    
         setCurrentTimeOnHeader();
         checkTense();
-    // }, 1000);
+    
 
+    // var descrip = $(".description").val(notes)
+    // console.log(notes);
+    for (var i = 9; i < 18; i++) {
+        console.log(JSON.stringify(i))
+     var x = $("#hour-" + JSON.stringify(i) + " .description").val(savedTextObj["hour-" + JSON.stringify(i)]);
+        console.log(x)
+    }
+    
+    
+    
+    
 });
